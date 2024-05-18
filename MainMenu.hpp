c8aad8d4fe0bfea1078c6ce98d7d431a80ce467b
Mathew005 <mathewsaju210@gmail.com>
2024-05-18 11:31:03 +0530

    MainMenu - Exit

--- MainMenu.hpp
+++ MainMenu.hpp
@@ -12,6 +12,14 @@ class MainMenu: public Engine::State
 private:
 	std::shared_ptr<Context> m_context;
 	sf::Text m_gameTitle;
+	sf::Text m_playButton;
+	sf::Text m_exitButton;
+
+	bool m_isPlayButtonSelected;
+	bool m_isPlayButtonPressed;
+
+	bool m_isExitButtonSelected;
+	bool m_isExitButtonPressed;
 public:
 	MainMenu(std::shared_ptr<Context>& context);
 	~MainMenu();
